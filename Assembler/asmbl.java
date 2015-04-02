7df94f0569fdacb1a7aee98b0fe62848a72093f5
yzha3 <yzha3@wisc.edu>
2015-04-01 22:58:36 -0500

    Version 1.0: Support: Add, Sub, Xor, LD, SW, LH, LL, SHIFT, B(do not support flag)

---
+++ Assembler/asmbl.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+import java_cup.runtime.*;  // defines Symbol
+
+public class asmbl {
+    public static void main(String[] args) throws IOException {
+
+		if (args.length != 1) {
+			System.err.println("Usage: java asmbl <inputFile>");
+			System.exit(-1);
+		}
+		FileReader inFile = null;
+		try {
+			inFile = new FileReader(args[0]);
+		} catch (FileNotFoundException ex) {
+			System.err.println("File" + args[0] + " not found.");
+			System.exit(-1);
+		}
+
+		parser P = new parser(new Yylex(inFile));
+
+		Symbol root = null;
+
+		try {
+			root = P.parse();
+		} catch (Exception ex) {
+			System.err.println("Exception occured during parse: "+ ex);
+			System.exit(-1);
+		}
+
+		((ASTnode)root.value).translate();
+		return;
+    }
+}

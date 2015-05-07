df04e881ead639e95e673b6589d243c7c3231121
yzha3 <yzha3@wisc.edu>
2015-05-07 13:17:28 -0500

    Version 1.1: Support all instructions

--- CPU_Simulator/simulator.java
+++ CPU_Simulator/simulator.java
@@ -15,6 +15,8 @@ public class simulator {
 
 		// read instructions
 		Memory = new HashMap<Integer, String>();
+		Memory.put(4094, "0000");
+		Memory.put(4095, "0000");
 		String s;
 		int addr = 0x0000;
 		while ((s= br.readLine()) != null) {
@@ -37,6 +39,9 @@ public class simulator {
 		// start running
 		String instr;
 		int dst, src0, src1, temp;
+		int outputflag = 0;
+		int outputcount = 0;
+		int outputsum = 0;
 		String data;
 		while (addr != -1) {
 			instr = Memory.get(addr);
@@ -44,7 +49,7 @@ public class simulator {
 				System.err.println("Error @"+Integer.toString(addr, 16));
 			 	break;
 			}
-		//	System.out.println(instr);
+		//	System.err.println(Integer.toString(addr,16) +" "+instr);
 			switch(instr.charAt(0)){
 				default: System.err.println("Unknown instruction @" + Integer.toString(addr,16));
 						 System.exit(-1);
@@ -129,6 +134,8 @@ public class simulator {
 					break;
 				case '3': // LD
 					if (instr.charAt(1) != '0') {
+						if (Register[Integer.valueOf(instr.substring(2,3),16)] < 1000) 
+							System.out.println(Integer.toString(addr,16) + " "+ instr+ " "+"illegal " + Register[Integer.valueOf(instr.substring(2,3),16)]);
 						data = Memory.get(Register[Integer.valueOf(instr.substring(2,3),16)]);
 						if (data == null) temp = 0;
 						else temp = Integer.valueOf(data, 16);
@@ -137,9 +144,9 @@ public class simulator {
 					addr++;
 					break;
 				case '4': //SW
+						if (Register[Integer.valueOf(instr.substring(2,3),16)] < 1000) 
+							System.out.println(Integer.toString(addr,16) + " "+ instr+ " "+"illegal");
 						String store = Integer.toString(Register[Integer.valueOf(instr.substring(1,2),16)],16);
-						for (int i=4; i>store.length(); i--)
-							store ="0"+store;
 						Memory.put(Register[Integer.valueOf(instr.substring(2,3),16)], store);
 						addr++;
 						break;
@@ -251,18 +258,51 @@ public class simulator {
 				case 'a':
 					addr = Register[Integer.valueOf(instr.substring(1,2),16)];
 					break;
+				case 'b':
+					System.out.println("Accelerator Control instruction: " +instr);
+					addr++;
+					break;
 				case 'c':
-					if (instr.charAt(3) == '2'){
-						int x= Integer.valueOf(instr.substring(1,3),16);
-						if (x == 0x0d)
-							System.out.println();
-						else
-							System.out.print((char)x);
-					}
-					else if (instr.charAt(3) == '1')
-						System.out.print((char)(int)(Register[Integer.valueOf(instr.substring(1,2),16)]/256));
+					int x;
+					if (instr.charAt(3) == '2')
+						x= Integer.valueOf(instr.substring(1,3),16);
+					else if (instr.charAt(3) == '1') 
+						x = (int)(Register[Integer.valueOf(instr.substring(1,2),16)]/256);
 					else
-						System.out.print((char)(int)(Register[Integer.valueOf(instr.substring(1,2),16)]%256));
+						x = (int)(Register[Integer.valueOf(instr.substring(1,2),16)]%256);
+
+					if (x == 0xfe && (outputflag == 0||outputcount == 2)) {
+						outputflag = 1-outputflag;
+						outputcount = 0;
+						outputsum = 0;
+					}
+					else if (x == 0xff && (outputflag == 0 || outputcount == 2)) {
+						outputflag = 2 - outputflag;
+						outputcount = 0;
+						outputsum = 0;
+					}
+					else if (outputflag == 1) {
+						outputcount ++;
+						if (outputcount == 1)
+							outputsum = x;
+						else {
+							outputsum *= 256;
+							outputsum += x;
+						}
+						if (outputcount == 2)
+						System.out.print((outputsum > 32767)?(outputsum - 65536):outputsum);
+					}
+					else if (outputflag == 2) {
+						outputcount ++;
+						if (Integer.toString(x,16).length() == 0) System.out.print("00");
+						if (Integer.toString(x,16).length() == 1) System.out.print("0");
+						System.out.print(Integer.toString(x, 16));
+					}
+					else if (x == 0x0d) {
+						System.out.println();
+					}
+					else
+						System.out.print((char)x);
 					addr++;
 					break;
 				case 'd': // set
@@ -272,11 +312,47 @@ public class simulator {
 					else
 						addr ++;
 					break;
+				case 'f': // addi do not update flag
+					if (instr.charAt(1) != '0'){
+						src1 = Integer.valueOf(instr.substring(3),16);
+						if (src1>7) src1 -= 16;
+						src0 = Register[Integer.valueOf(instr.substring(2,3),16)];
+						if (src0 > 32767) src0 -= 65536;
+						dst = src0 + src1;
+						if (dst < -32768) dst = -32768;
+						if (dst > 32767) dst = 32767;
+						if (dst < 0) dst += 65536;
+						Register[Integer.valueOf(instr.substring(1,2),16)] = dst;
+					}
+					addr++;
+					break;
+			}
+			if (addr == -1) {
+				System.out.println(">  Program Finished or Sleeped");
+				System.out.print(">  Do you want to wake up program?");
+				BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+				String inputString = in.readLine();
+				if (inputString.equals("yes")) {
+					// Search for sleep program
+					for ( int i = 0x1006; i < 0xffff; i+= 0x4000){ 
+						if (Memory.get(i) != null) {
+							addr = Integer.valueOf(Memory.get(i), 16);
+							if (addr != 0) {
+								System.out.println("wake up to " + addr);
+								break;
+							}
+							else
+								addr = -1;
+						}
+					}
+					if ( addr == -1)
+						System.out.println(">  No program is sleeping");
+				}
 			}
 		}
 
 		// dump register
-		System.out.println("\nRegister Dump");
+		System.out.println("=============================\nRegister Dump");
 		for (int i = 0; i<16; i++) {
 			System.out.print("R"+Integer.toString(i,16) + ": 0x");
 			String result = Integer.toString(Register[i],16);
@@ -284,8 +360,6 @@ public class simulator {
 				System.out.print("0");
 			System.out.println(result);
 		}
-		return;
-
     }
 
 	static private HashMap<Integer, String> Memory = null;
